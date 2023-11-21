9cd49a35cbe5779f681e754a73c363357869c5ee
mohamed1998 <mohamed.saad.tradelinestores@gmail.com>
2023-11-21 14:40:15 +0200

    first commit

---
+++ components/Button.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import React from 'react'
+type ButtonProps = {
+    type: "button" | "submit"
+    icon?: string;
+    title: string;
+    variant: string
+    full?: boolean
+}
+const Button = ({type, title, icon, variant, full}: ButtonProps) => {
+return (
+  <button
+    type={type}
+    className={`flexCenter gap-3 rounded-full border  ${variant} ${
+      full && 'w-full'
+    }`}>
+    {icon && (
+      <Image
+        src={icon}
+        alt="title"
+        width={24}
+        height={24}
+        className="bold-16 whitespace-nowrap"
+      />
+    )}
+    <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
+  </button>
+);
+}
+
+export default Button
